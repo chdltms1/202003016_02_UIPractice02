a2c2e1beebcb1092f9a8bd4fae59043e5bcee9ee
chdltms1 <61855480+chdltms1@users.noreply.github.com>
2020-03-16 16:15:27 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a202003016_02_uipractice/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.a202003016_02_uipractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
